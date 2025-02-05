315f9117b5ede220e17fcf9a5ecd83fd4e5fa6ee
DharmaRckah <dprajapati15302@gmail.com>
2025-02-05 19:26:54 +0530

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Register from './components/Register'
+import Login from './components/Login'
 function App() {
 
 
@@ -11,6 +12,7 @@ function App() {
   <BrowserRouter>
    <Routes>
     <Route path ="/register" element={<Register/>}/>
+    <Route path ="/login" element={<Login/>}/>
 
    </Routes>
   </BrowserRouter>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
+import useAuth from '../hooks/useAuth'
 
 const Login = () => {
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
     const [error,setError]=useState("")
-    
+    const {login}=useAuth()
   return (
-    <div>
-      
+    <div className='flex items-center justify-center h-screen'>
+      <form action="">
+        <h2>Login</h2>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Password' />
+        <button>Login</button>
+      </form>
     </div>
   )
 }
